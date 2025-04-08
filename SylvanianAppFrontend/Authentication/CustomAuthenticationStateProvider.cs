6a7268ec79aa666520a235f7684b61532e3d5657
mais4g <137334729+mais4g@users.noreply.github.com>
2025-04-08 18:21:57 -0300

    .

---
+++ SylvanianAppFrontend/Authentication/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,51 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SylvanianAppFrontend.Authentication
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
+
+        public CustomAuthenticationStateProvider(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var isLoggedIn = await _localStorage.GetItemAsync<bool>("isLoggedIn");
+
+            ClaimsIdentity identity;
+            if (isLoggedIn)
+            {
+                // Aqui você pode adicionar claims reais do usuário (ex: nome, email, roles, etc)
+                identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, "UsuárioDemo")
+                }, "FakeAuthType");
+            }
+            else
+            {
+                identity = new ClaimsIdentity();
+            }
+
+            var user = new ClaimsPrincipal(identity);
+            return new AuthenticationState(user);
+        }
+
+        public async Task LoginAsync()
+        {
+            await _localStorage.SetItemAsync("isLoggedIn", true);
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+
+        public async Task LogoutAsync()
+        {
+            await _localStorage.RemoveItemAsync("isLoggedIn");
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+    }
+}
--- SylvanianAppFrontend/Program.cs
+++ SylvanianAppFrontend/Program.cs
@@ -1,10 +1,13 @@
 using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using SylvanianAppFrontend;
+using SylvanianAppFrontend.Authentication;
 using System;
 using System.Net.Http;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -13,6 +16,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5261/api/") });
 
-
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 
 await builder.Build().RunAsync();
